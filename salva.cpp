222979e0d636457910fec8128ba47bb85905cb37
shawnnqt <shawnsalva80@gmail.com>
2026-04-05 23:14:20 +0800

    Update salva.cpp

---
+++ salva.cpp
@@ -0,0 +1,24 @@
+#include <unordered_set>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_set<char> chars;
+        int left = 0, maxLength = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+
+            while (chars.count(s[right])) {
+                chars.erase(s[left]);
+                left++;
+            }
+
+            chars.insert(s[right]);
+            maxLength = max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
+    }
+};
